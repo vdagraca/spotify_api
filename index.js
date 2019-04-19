2938efdd1e089d9481c7232f1c568efafd09e104
Vivian da Graca <vivian.da.graca@gmail.com>
2019-04-19 20:19:29 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+// const customersRouter = require('./customers/routes')
+
+const app = express()
+const port = process.env.PORT || 4001
+
+app
+    .use(bodyParser.json())
+    // .use(customersRouter)
+    .listen(port, () => console.log(`Listening on port ${port}`))
